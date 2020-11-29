07a8fbd9454b2c8cb9242e8f103b9ad2bcc8c4cb
Afeer Mohd <afeerr@gmail.com>
2020-11-29 10:43:41 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA9WRYh1hBN1v5k-dgVMXk42dHmHdtfiyo",
+  authDomain: "ama-zon-afeer.firebaseapp.com",
+  databaseURL: "https://ama-zon-afeer.firebaseio.com",
+  projectId: "ama-zon-afeer",
+  storageBucket: "ama-zon-afeer.appspot.com",
+  messagingSenderId: "345031673275",
+  appId: "1:345031673275:web:dae902a06eadbd5351709e",
+  measurementId: "G-6N9D2PLBH4",
+};
